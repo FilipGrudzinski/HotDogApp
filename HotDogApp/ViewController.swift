0e444a893d45031d558588e72cb0447517182110
FilipGrudzinski <grudzinski.filip@gmail.com>
2018-11-25 20:19:05 +0100

    Initial Commit

---
+++ HotDogApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotDogApp
+//
+//  Created by Filip on 25/11/2018.
+//  Copyright © 2018 Filip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
